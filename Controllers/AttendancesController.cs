7ea9d41278818bae96ecad5e29402f6bfa340cbf
truong <quangtruong.tdr012@gmail.com>
2021-07-13 10:17:01 +0700

    Mine

---
+++ Controllers/AttendancesController.cs
@@ -0,0 +1,29 @@
+﻿using BigSchool.Models;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace BigSchool.Controllers
+{
+    public class AttendancesController : ApiController
+    {
+        [HttpPost]
+        public IHttpActionResult Attend(Course attandanceDto)
+        {
+            var userID = User.Identity.GetUserId();
+            BigSchoolContext context = new BigSchoolContext();
+            if (context.Attendances.Any(p => p.Attendee == userID && p.CourseId == attandanceDto.id))
+            {
+                return BadRequest("The attendance already exists!");
+            }
+            var attendance = new Attendance() { CourseId = attandanceDto.id, Attendee = User.Identity.GetUserId() };
+            context.Attendances.Add(attendance);
+            context.SaveChanges();
+            return Ok();
+        }
+    }
+}
